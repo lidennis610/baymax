c3906f2e20f0da768fe5eb23e21ccfbc271bc968
Dennis Li <62808150+lidennis610@users.noreply.github.com>
2022-04-21 18:36:31 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import Constants from 'expo-constants'
+import * as Permissions from 'expo-permissions'
+imageToTensor(rawImageData) {
+    const TO_UINT8ARRAY = true
+    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
+    // Drop the alpha channel info for mobilenet
+    const buffer = new Uint8Array(width * height * 3)
+    let offset = 0 // offset into original data
+    for (let i = 0; i < buffer.length; i += 3) {
+      buffer[i] = data[offset]
+      buffer[i + 1] = data[offset + 1]
+      buffer[i + 2] = data[offset + 2]
+
+      offset += 4
+    }
+
+    return tf.tensor3d(buffer, [height, width, 3])
+  }
